0ff1767533c5152131abe8b1d2f4c0110c5e39d9
Jonagon16 <jonadanielgonzalez@gmail.com>
2023-08-17 21:31:32 -0300

    completo

---
+++ modulo5/ejercicio4.py
@@ -0,0 +1,22 @@
+z = False
+d = []
+
+
+while not z:
+    try:
+        fs = input("Ingrese el nombre del archivo que quiere abrir: ")
+        with open(fs,"r") as f:
+            fd = f.readlines()
+            d = fd
+        with open(fs, "w") as f:
+            f.write("")
+        for i in range(len(d)):
+            d[i] = d[i][::-1].strip("\n")
+            with open(fs,"a") as f:
+                f.write(f"{d[i]}\n")
+                z = True
+
+    except FileNotFoundError:
+        print("Archivo no encontrado.")
+
+print(f"el archivo quedario asi: {d} ")
