62b80b541e4487e1af7120e0ea95367d8bd5da8c
Jonagon16 <jonadanielgonzalez@gmail.com>
2023-09-09 05:31:06 -0300

    sag terminado falta funciones soon

--- Proyecto Scam-Sag/main.py
+++ Proyecto Scam-Sag/main.py
@@ -1,29 +1,161 @@
-def recibir_antecedente():
-    ant={}
-    with open("antecedente_salida.txt", 'r') as archivo:
-        for linea in archivo:
-            partes = linea.split(' : ')
-            if len(partes) == 2:
-                clave, valor = partes
-                ant[clave.strip()] = valor
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from whatsapp import *
+from Scam_SAG import *
+
+def accion_dominio():
+    print("Último antecedente:", mensaje_actual)
+
+    time.sleep(1)
+    m1, m2 = pedir_antecedente_dominio(mensaje_actual[0].strip("\n"),driver)
+    datosDominio = antecedente_dic(m1, m2)
+    resultado = antecedente_dominio_final(datosDominio)
+    print(resultado)
+    driver.switch_to.window(driver.window_handles[0])
+    time.sleep(1)
+    enviar_mensaje(resultado, driver)
+    time.sleep(1)
+    imprimir_antecedente(datosDominio)
+    enviar_antecedente(datosDominio, driver)
+
+
+def accion_dni1():
+    print("Último antecedente:", mensaje_actual)
+    driver.get('https://sag.gna/PER_Busqueda.aspx')
+    time.sleep(1)
+    m1 = pedir_antecedente_dni(mensaje_actual[1].strip("\n"),driver)
+    print(m1)
+    driver.switch_to.window(driver.window_handles[0])
+    time.sleep(1)
+    enviar_mensaje(m1, driver)
+    time.sleep(1)
+
+def accion_dni2():
+    print("Último antecedente:", mensaje_actual)
+
+    driver.switch_to.window(driver.window_handles[1])
+    driver.get('https://sag.gna/PER_Busqueda.aspx')
+    time.sleep(1)
+    m1 = pedir_antecedente_dni(mensaje_actual[1].strip("\n"), driver)
+    print(m1)
+    driver.switch_to.window(driver.window_handles[0])
+    time.sleep(1)
+    enviar_mensaje(m1, driver)
+    time.sleep(1)
+
+mensaje_formato = """"*FORMATOS PERMITIDOS:*
+*EJEMPLO 1:*
+ABC123
+40211345
+*EJEMPLO 2:*
+ABC123
+*EJEMPLO 3:*
+40211345
+*EJEMPLO 4:*
+LS
+"""
+ultimo_mensaje = ""
+x = False
+y = False
+z = False
+
+# Ruta al archivo de configuración de cookies
+CONFIG_FILEW = "Wconfig.ini"
+CONFIG_FILES = "Sconfig.ini"
+# Función para mostrar el menú
+def mostrar_menu():
+    print("\nMENU:")
+    print("1. Iniciar SAG")
+    print("2. LOGS ANTECEDENTES(SOON)")
+    print("3. ANTECEDENTES CON NOVEDAD SOON")
+    print("4. SALIR")
+
+while not y:
+    mostrar_menu()
+    opcion=input("")
+    if opcion == "1":
+        y = True
+
+    elif opcion == "2":
+        print("Ver Logs de Antecedentes:")
+        log_file = "antecedente_salida.txt"
+        if os.path.exists(log_file) and os.path.getsize(log_file) > 0:
+            with open(log_file, 'r') as file:
+                logs = file.read()
+            print(logs)
+        else:
+            print("No hay registros de antecedentes disponibles.")
+
+    elif opcion == "3":
+        print("Ver Logs de Antecedentes:")
+        log_file = "antecedente_salida.txt"
+        if os.path.exists(log_file) and os.path.getsize(log_file) > 0:
+            with open(log_file, 'r') as file:
+                logs = file.read()
+            print(logs)
+        else:
+            print("No hay registros de antecedentes disponibles.")
+
+    elif opcion == "4":
+        print("Saliendo del programa")
+        exit()
+    else:
+        print("Opción inválida.")
+#MAIN
+driver = webdriver.Chrome()
+driver.get("https://web.whatsapp.com/")
+print("Iniciando Whatsapp.")
+#verifica si el whatsapp tiene cookies o no
+if os.path.exists(CONFIG_FILEW) and os.path.getsize(CONFIG_FILEW) > 0:
+    cargar_cookies(driver, CONFIG_FILEW)
+else:
+    input("Por favor escanea el codigo QR")
+    guardar_cookies(driver,CONFIG_FILEW)
+#inicia sag
+driver.execute_script("window.open('', '_blank');")
+time.sleep(3)
+driver.switch_to.window(driver.window_handles[1])
+time.sleep(1)
+driver.get('https://sag.gna/login.aspx?ReturnUrl=%2fFrame_Principal.aspx%3fidpag%3d92%26fullscreen%3dFalse&idpag=92&fullscreen=False')
+input("Aprete Enter una vez este cargado")
+loguear_antecedente(driver)
+time.sleep(3)
+driver.switch_to.window(driver.window_handles[0])
+time.sleep(1)
+abrir_contacto(driver)
+while not z:
+    if mensaje_en_pantalla(driver):
+        mensaje_actual = obtener_ultimo_mensaje(driver)
+        if mensaje_actual[0] == "antecedentes":
+            z = True
+    else:
+        time.sleep(5)
+while not x:
+    if mensaje_en_pantalla(driver):
+        mensaje_actual = obtener_ultimo_mensaje(driver)
+        if mensaje_actual != ultimo_mensaje:
+            mensaje_actual = obtener_ultimo_mensaje(driver)
+            if len(mensaje_actual) == 3:
+                accion_dominio()
+                accion_dni1()
+                ultimo_mensaje = mensaje_actual
+            elif len(mensaje_actual) == 2:
+                if int(mensaje_actual[1]):
+                    accion_dni2()
+                    ultimo_mensaje = mensaje_actual
+                elif mensaje_actual[0] == "LS":
+                    break
+                else:
+                    accion_dominio()
+                    ultimo_mensaje = mensaje_actual
             else:
-                print(f"Formato incorrecto en la línea: {linea.strip()}")
-    return ant
-def formatear_mensaje_reducido(diccionario):
-    claves_mostradas = ["Dominio", "Motor", "Chasis", "Marca", "Modelo", "Titular", "Denuncia"]
-    mensaje = ""
-    for clave in claves_mostradas:
-        valor = diccionario.get(clave, "")  # Obtén el valor de la clave o cadena vacía si no existe
-        if valor.strip() != "":
-            mensaje += f"{clave} : {valor}\n"
+                print(mensaje_actual)
+                enviar_mensaje(mensaje_formato,driver)
+                ultimo_mensaje = mensaje_actual
 
-    # Elimina el último carácter de nueva línea adicional si es necesario
-    if mensaje.endswith('\n'):
-        mensaje = mensaje[:-1]
-
-    return mensaje
-
-a = recibir_antecedente()
-b= formatear_mensaje_reducido(a)
-print(a)
-print(b)
+    else:
+        time.sleep(3)
--- Proyecto Scam-Sag/whatsapp.py
+++ Proyecto Scam-Sag/whatsapp.py
@@ -5,8 +5,49 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import os
 import time
-driver = webdriver.Chrome()
-driver.get("https://web.whatsapp.com/")
+
+
+
+CONFIG_FILE = "config.ini"
+
+def iniciar_whatsapp():
+    """
+    Inicia el sistema SAG.
+    Verifica si el archivo de configuración contiene cookies de sesión.
+    Si contiene cookies, carga las cookies y abre WhatsApp Web.
+    Si el archivo está vacío, escanea el código QR y guarda las cookies en el archivo.
+    """
+    if os.path.exists(CONFIG_FILE) and os.path.getsize(CONFIG_FILE) > 0:
+        # Si el archivo existe y no está vacío, cargar las cookies
+        cargar_cookies()
+    else:
+        pass
+
+
+def guardar_cookies(driver,cf):
+    """
+    Guarda las cookies de sesión en el archivo de configuración.
+    """
+    cookies = driver.get_cookies()
+
+    # Crear un archivo de configuración (INI) y guardar las cookies
+    with open(cf, 'w') as config_file:
+        for cookie in cookies:
+            config_file.write(f"{cookie['name']}={cookie['value']}\n")
+def cargar_cookies(driver,cf):
+    """
+    Carga las cookies de sesión desde el archivo de configuración.
+    """
+    with open(cf, 'r') as config_file:
+        cookies = [line.strip().split('=') for line in config_file.readlines()]
+        for cookie in cookies:
+            if len(cookie) == 2:
+                driver.add_cookie({'name': cookie[0], 'value': cookie[1]})
+
+    # Actualiza la página para cargar las cookies
+    print(cookies)
+    driver.refresh()
+
 
 def imprimir_antecedente(dic):
     max_clave = max(len(clave) for clave in dic.keys())
@@ -30,64 +71,60 @@ def formatear_mensaje_reducido(diccionario):
         clave = clave.strip()
         valor = diccionario.get(clave, "")  # Obtén el valor de la clave o cadena vacía si no existe
         if valor.strip() != "":
-            mensaje += f"{clave} : {valor}\n"
+            mensaje += f"*{clave} =* {valor}\n"
 
     # Elimina el último carácter de nueva línea adicional si es necesario
     if mensaje.endswith('\n'):
         mensaje = mensaje[:-1]
 
     return mensaje
-def obtener_ultimo_mensaje():
-    #ultimo mensaje
+def obtener_ultimo_mensaje(driver):
     ultimoMensaje = driver.find_elements(by="css selector", value="div.message-in")[-1].text
     antecedente = ultimoMensaje.split("\n")
-    with open("antecedente_entrada.txt","w") as ant_ent:
-        ant_ent.write(antecedente[0])
-    return antecedente
+    if len(antecedente) == 3:
+        return antecedente
+    elif len(antecedente) == 2:
+        return antecedente
+    else:
+        return ultimoMensaje
 
-def enviar_antecedente(dic):
+
+def mensaje_en_pantalla(driver):
+    try:
+        ultimoMensaje = driver.find_elements(by="css selector", value="div.message-in")[-1].text
+        return True
+    except IndexError:
+        return False
+
+def enviar_antecedente(dic,driver):
     mensaje = formatear_mensaje_reducido(dic)
     input_box = driver.find_element(By.XPATH,'//div [@title="Escribe un mensaje"]')
     input_box.send_keys(mensaje)
     input_box.send_keys(Keys.RETURN)
+def enviar_mensaje(mensaje,driver):
+    input_box = driver.find_element(By.XPATH,'//div [@title="Escribe un mensaje"]')
+    input_box.send_keys(mensaje)
+    input_box.send_keys(Keys.RETURN)
 
-def recibir_antecedente():
-    ant={}
-    with open("antecedente_salida.txt", 'r') as archivo:
-        for linea in archivo:
-            partes = linea.split(' : ')
-            if len(partes) == 2:
-                clave, valor = partes
-                ant[clave] = valor
-            else:
-                print(f"Formato incorrecto en la línea: {linea.strip()}")
-    with open("antecedente_salida.txt", "w") as ant_file:
-        ant_file.truncate()
+def recibir_antecedente(ant):
+    for linea in ant:
+        partes = linea.split(' : ')
+        if len(partes) == 2:
+            clave, valor = partes
+            ant[clave] = valor
+        else:
+            print(f"Formato incorrecto en la línea: {linea.strip()}")
     return ant
 
 
+def abrir_contacto(driver):
+    webdriver.ActionChains(driver).key_down(Keys.CONTROL).send_keys('2').key_up(Keys.CONTROL).perform()
+    time.sleep(5)
+    search_box = driver.find_element(by="css selector", value="div[contenteditable='true']")
+    search_box.send_keys("INSECTA")
+    time.sleep(2)
+    contacto = "span[title='INSECTA']"
+    driver.find_element(by="css selector", value=contacto).click()
+    time.sleep(2)
 
-input("Escanea el código QR y presiona Enter para continuar...")
-#abre la lista de contactos
-webdriver.ActionChains(driver).key_down(Keys.CONTROL).send_keys('2').key_up(Keys.CONTROL).perform()
-time.sleep(5)
-#busca el contacto
-search_box = driver.find_element(by="css selector", value="div[contenteditable='true']")
-search_box.send_keys("INSECTA")
-time.sleep(2)
-contacto = "span[title='INSECTA']"
-driver.find_element(by="css selector", value=contacto).click()
-time.sleep(2)
-ultimo_mensaje = ""
-mensaje_actual = obtener_ultimo_mensaje()
-while True:
-
-    if mensaje_actual != ultimo_mensaje:
-        mensaje_actual = obtener_ultimo_mensaje()
-        print("Último antecedente:", mensaje_actual)
-        antecedente = recibir_antecedente()
-        print(antecedente)
-        enviar_antecedente(antecedente)
-        ultimo_mensaje = mensaje_actual
-    time.sleep(1)
 
